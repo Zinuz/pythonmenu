961b50539daeffbfb0924a0527204c09d559deb7
Zinuz <zinuzmail@gmail.com>
2015-10-22 18:35:53 +0200

    Commit

--- terminal/config.py
+++ terminal/config.py
@@ -3,5 +3,5 @@ application_version = "0.0.0"
 
 menu_options = [[1, "Something One", "somethingone()"],
                 [2, "Something Two", "somethingtwo()"],
-                [3, "Something Three", "somethingthree()"]
+                [3, "Something Three", "somethingthree()"],
                 [4, "Exit Application", "exitapplication()"]]
