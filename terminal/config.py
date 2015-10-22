8eeb6cb34582efba87e7b1b6e9857685abbd7dd2
Zinuz <zinuzmail@gmail.com>
2015-10-22 18:25:28 +0200

    Commit

--- terminal/config.py
+++ terminal/config.py
@@ -1,1 +1,7 @@
-__author__ = 'admin'
+application_name = "Placeholder Name"
+application_version = "0.0.0"
+
+menu_options = [[1, "Something One", "somethingone()"],
+                [2, "Something Two", "somethingtwo()"],
+                [3, "Something Three", "somethingthree()"]
+                [4, "Exit Application", "exitapplication()"]]
