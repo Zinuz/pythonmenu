2e5a5218493638907aee3265d2945e5b226e1609
Zinuz <zinuzmail@gmail.com>
2015-10-22 18:48:13 +0200

    Commit

--- terminal/config.py
+++ terminal/config.py
@@ -1,7 +1,7 @@
 application_name = "Placeholder Name"
 application_version = "0.0.0"
 
-menu_options = [[1, "Something One", "somethingone()"],
-                [2, "Something Two", "somethingtwo()"],
-                [3, "Something Three", "somethingthree()"],
-                [4, "Exit Application", "exitapplication()"]]
+menu_options = [[1, "Something One", ""],
+                [2, "Something Two", ""],
+                [3, "Something Three", ""],
+                [4, "Exit Application", ""]]
